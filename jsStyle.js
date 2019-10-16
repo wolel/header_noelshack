d4d96d9870810da60f52e175a07e4a71bb5685e8
wolel <56022602+wolel@users.noreply.github.com>
2019-10-16 16:02:22 +0200

    reconstitution site Simplon avec modifs

--- jsStyle.js
+++ jsStyle.js
@@ -1,1 +1,14 @@
-/*animation: 3s ease-in 1s 2 reverse both paused slidein;
+anime.timeline({loop: true})
+    .add({
+        targets: '.titre',
+        opacity: [0,2],
+        easing: "easeInOutQuad",
+        duration: 2250,
+        delay: (el, i) => 150 * (i+1)
+    }).add({
+    targets: '.titre',
+    opacity: 3,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+});
